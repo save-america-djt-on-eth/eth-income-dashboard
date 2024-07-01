c4fe02eae760e4fa0819c369f0867d79a83b0010
save-america-djt-on-eth <picantri@proton.me>
2024-07-01 13:33:01 -0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,11 @@ module.exports = {
       name: 'backend',
       script: './backend/index.js',
       watch: true,
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+        INFURA_API_KEY: 'your_infura_api_key'
+      }
     },
     {
       name: 'frontend',
