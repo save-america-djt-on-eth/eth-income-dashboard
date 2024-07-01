096085cc65d2bc943068f8183991675864513815
save-america-djt-on-eth <picantri@proton.me>
2024-07-01 13:47:15 -0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,9 @@
-require('dotenv').config();
-
 module.exports = {
   apps: [
     {
       name: 'backend',
       script: './backend/index.js',
       watch: true,
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3000,
-        INFURA_API_KEY: process.env.INFURA_API_KEY
-      }
     },
     {
       name: 'frontend',
