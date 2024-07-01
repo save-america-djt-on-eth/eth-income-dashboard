a8127153c2a0363fc73d04ce57463fff28184640
save-america-djt-on-eth <picantri@proton.me>
2024-07-01 16:28:55 -0500

    Update scripts.js

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -54,7 +54,7 @@ function updateChart(labels, supplyChange, contractBalance) {
                     },
                     adapters: {
                         date: {
-                            locale: window.dateFnsLocale // Use the global date-fns locale
+                            locale: window.dateFnsLocale // Ensure the locale is correctly referenced
                         }
                     }
                 }
