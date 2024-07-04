298c85ff2a3e4ff4dd21764bfbeb9951f5270bdd
save-america-djt-on-eth <picantri@proton.me>
2024-07-04 09:28:41 -0500

    Delete backend/routes.js

--- backend/routes.js
+++
@@ -1,8 +0,0 @@
-const { getData, getCache } = require('./cache');
-
-const setupRoutes = (app) => {
-  app.get('/api/data', getData);
-  app.get('/api/cache', getCache);
-};
-
-module.exports = { setupRoutes };
