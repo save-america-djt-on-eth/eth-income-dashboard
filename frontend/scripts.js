55cb438e3abe233472e0a8c0a87dd0587d76d0b4
save-america-djt-on-eth <picantri@proton.me>
2024-07-06 09:43:14 -0500

    Update scripts.js

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -32,8 +32,8 @@ function fetchData(timeFrame) {
             console.log("API Data: ", data);
             updateChart(data.labels, data.trumpEtherIncomeDuringTimeFrame, data.etherIncomeFromContract, timeFrame);
             document.getElementById("total-eth").innerText = Number(data.trumpTotalEther).toFixed(4);
-            document.getElementById("eth-generated-djt").innerText = Number(data.totalEtherFromDJT.slice(-1)[0]).toFixed(4);
-            const percentage = ((Number(data.totalEtherFromDJT.slice(-1)[0]) / Number(data.trumpTotalEther)) * 100).toFixed(0);
+            document.getElementById("eth-generated-djt").innerText = Number(data.etherIncomeFromContract.slice(-1)[0]).toFixed(4);
+            const percentage = ((Number(data.etherIncomeFromContract.slice(-1)[0]) / Number(data.trumpTotalEther)) * 100).toFixed(0);
             document.getElementById("eth-percentage-value").innerText = `${percentage}%`;
         })
         .catch(error => console.error("Error fetching data: ", error));
